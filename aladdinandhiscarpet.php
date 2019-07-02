a996b814d255bd6dc3be3418ec66d3624dfc9678
ILKER ADIYAMAN <ilkerburak@hotmail.com>
2019-07-03 02:50:29 +0300

    update

--- aladdinandhiscarpet.php
+++ aladdinandhiscarpet.php
@@ -1,28 +1,27 @@
 <?php
+
 function optimalPoint($magic, $dist) {
-    $index = 0; // magic index
-    $visited = 0;// visited counter, if equals to total distance return magic index
-    $try = 0;  // counter to make sure we tried all magic sources
+    for($start = 0; $start < count($magic); $start++){ //try count
+        $curMagic = $magic[$start];
+        for ($i = 0; $i < count($dist); $i++) {
+            $start++;
+            if($start == count($magic)) $start = 0;
+            $curMagic = $curMagic + $magic[$start] - $dist[$i];
 
-    while ($try < count($magicArr)){ //try magic count  times
-        if($visited == count($dist)) return $index;
-        $visited++;
-
-        for($j = 0; $j < count($dist); $j++) {
-            $tmpMagic = $magicArr[$index];
-            $tmpMagic += $magicArr[$index + 1] - $dist[$j+ 1];
-            if($tmpMagic < $dist[$visited + 1]) break;  // start with next source
+            if ($dist[$i] > $curMagic) {
+                if($start != count($magic) -1) $start--;
+                continue 2;
+            }
         }
-
-        $index++;
-        if($index == count($magicArr)) $index = 0;
-        $try++;
+        if($curMagic >= 0) {
+            return $start;
+        }
     }
 
     return -1;
 }
 
-$magicArr = [2, 4, 5, 2];
-$distArr = [4, 3, 1, 3];
+$magicArr = [8,4,1,9];
+$distArr = [10,9,3,5];
 
 echo optimalPoint($magicArr, $distArr);
