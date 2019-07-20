beb710b17fd387c79cd1e60e7b0f5c66f42a05b7
ILKER ADIYAMAN <ilkerburak@hotmail.com>
2019-07-20 22:12:54 +0300

    solution

---
+++ equalizearray.php
@@ -0,0 +1,5 @@
+<?php
+/*
+ * Karl has an array of integers. He wants to reduce the array until all remaining elements are equal. Determine the minimum number of elements to delete to reach his goal.
+For example, if his array is , we see that he can delete the  elements  and  leaving . He could also delete both twos and either the  or the , but that would take  deletions. The minimum number of deletions is .
+*/
