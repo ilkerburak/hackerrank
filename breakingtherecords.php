a2441846762a264d422cc7f174442461bd5caf5f
ILKER ADIYAMAN <ilkerburak@hotmail.com>
2019-08-04 18:13:46 +0300

    code

---
+++ breakingtherecords.php
@@ -0,0 +1,27 @@
+<?php
+// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+
+function breakingRecords($scores) {
+    $countHighest = 0;
+    $countLowest = 0;
+    $tmpScores = [];
+
+    for($i = 0; $i < count($scores); $i++){
+        if(!isset($highest) && !isset($lowest)){
+            $highest = $scores[$i];
+            $lowest = $scores[$i];
+            $tmpScores[] = $scores[$i];
+        } else {
+            sort($tmpScores);
+            if($scores[$i] < $tmpScores[0]) {
+                $countLowest++;
+            } else if($scores[$i] > $tmpScores[count($tmpScores) - 1]) {
+                $countHighest++;
+            }
+
+            $tmpScores[] = $scores[$i];
+        }
+    }
+
+    return [$countHighest, $countLowest];
+}
